6ffb8d7349fa4abcefb6809e5a0b046987d6a316
Raj Kiran Acharyya <rajkiranacharyya@gmail.com>
2026-07-10 22:39:31 +0530

    Finalize implementation of AinotesapiApplication.java

--- src/main/java/com/example/ainotesapi/AinotesapiApplication.java
+++ src/main/java/com/example/ainotesapi/AinotesapiApplication.java
@@ -9,3 +9,6 @@ public class AinotesapiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AinotesapiApplication.class, args);
 		System.out.println("Server is running");
+	}
+
+}
