09d9aacf591610ac54e5d3dfe60e341e4d5a5043
Raj Kiran Acharyya <rajkiranacharyya@gmail.com>
2026-07-10 22:39:45 +0530

    Initialize AinotesapiApplicationTests.java

---
+++ src/test/java/com/example/ainotesapi/AinotesapiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.ainotesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AinotesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
