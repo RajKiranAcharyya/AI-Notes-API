a3c250c906a40835a349e8ee265200db051e136a
Raj Kiran Acharyya <rajkiranacharyya@gmail.com>
2026-07-10 22:39:36 +0530

    Update method signatures in GlobalExceptionHandler.java

--- src/main/java/com/example/ainotesapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/ainotesapi/exception/GlobalExceptionHandler.java
@@ -9,3 +9,14 @@ import org.springframework.web.client.HttpServerErrorException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoteNotFoundException.class)
+    public ResponseEntity<String> handleNoteNotFound(NoteNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(HttpServerErrorException.class)
+    public ResponseEntity<String> handleGemini(HttpServerErrorException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_GATEWAY);
+    }
+
