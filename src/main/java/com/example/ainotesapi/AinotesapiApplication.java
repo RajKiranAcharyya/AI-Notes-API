6c09b53d7bce8397e443419916b8b431c466b2b0
Raj Kiran Acharyya <rajkiranacharyya@gmail.com>
2026-07-10 22:39:31 +0530

    Initialize AinotesapiApplication.java

---
+++ src/main/java/com/example/ainotesapi/AinotesapiApplication.java
@@ -0,0 +1,11 @@
+package com.example.ainotesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AinotesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AinotesapiApplication.class, args);
+		System.out.println("Server is running");
