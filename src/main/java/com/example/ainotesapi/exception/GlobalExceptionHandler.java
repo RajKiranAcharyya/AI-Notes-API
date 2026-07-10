2b41f95f324062cb16dd624fcc71e0e3c3faeee5
Raj Kiran Acharyya <rajkiranacharyya@gmail.com>
2026-07-10 22:39:35 +0530

    Initialize GlobalExceptionHandler.java

---
+++ src/main/java/com/example/ainotesapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.example.ainotesapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpServerErrorException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
