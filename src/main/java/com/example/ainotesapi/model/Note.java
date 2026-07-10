dbbfcc71cbc64adf6d25cf72a79c927ecfe1e11e
Raj Kiran Acharyya <rajkiranacharyya@gmail.com>
2026-07-10 22:39:38 +0530

    Initialize local variables and class fields in Note.java

--- src/main/java/com/example/ainotesapi/model/Note.java
+++ src/main/java/com/example/ainotesapi/model/Note.java
@@ -42,3 +42,14 @@ public class Note {
     public String getTitle() {
         return title;
     }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
