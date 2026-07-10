5fe117b928ae2892157e2619e0b49777f07f2943
Raj Kiran Acharyya <rajkiranacharyya@gmail.com>
2026-07-10 22:39:34 +0530

    Resolve type mismatch warnings in RequestController.java

--- src/main/java/com/example/ainotesapi/controller/RequestController.java
+++ src/main/java/com/example/ainotesapi/controller/RequestController.java
@@ -75,3 +75,14 @@ public class RequestController {
             @RequestBody Note note) {
         // try {
         return new ResponseEntity<>(crudServices.patchUpdateRenameTitleAppendContent(id, note), HttpStatus.OK);
+        // } catch (RuntimeException e) {
+        // return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        // }
+    }
+
+    @DeleteMapping("/notes/deleteNote/{id}")
+    public ResponseEntity<String> deleteNote(@PathVariable Long id) {
+        // try {
+        return new ResponseEntity<>(crudServices.deleteANote(id), HttpStatus.OK);
+        // } catch (RuntimeException e) {
+        // return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
